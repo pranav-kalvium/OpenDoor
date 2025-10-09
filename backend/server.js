0b4e4feee19ad2393c31e13b442d558992ab924c
Pranav-cse-kalvium <pranav.prakash@kalvium.community>
2025-10-09 10:16:48 +0530

    backandfront

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // Connect to database
 connectDB();
 
-const allowedOrigins = [process.env.FRONTEND_URL || 'http://localhost:5173', 'http://localhost:5174'];
+const allowedOrigins = [process.env.FRONTEND_URL || 'http://localhost:5173', 'http://localhost:5174', 'https://opendooor.netlify.app' ];
 
 // app.use(cors({
 //   origin: function(origin, callback){
