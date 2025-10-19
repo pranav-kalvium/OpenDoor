53f77a4932cecb42d04f298e681538d50e85bd62
pranav-kalvium <pranavprakash6410@gmail.com>
2025-10-19 21:42:51 +0530

    Simplify CORS configuration in server.js
    Refactor CORS middleware to simplify origin handling.

--- backend/server.js
+++ backend/server.js
@@ -17,17 +17,7 @@ connectDB();
 const allowedOrigins = [process.env.FRONTEND_URL || 'http://localhost:5173', 'http://localhost:5174', 'https://opendooor.netlify.app' ];
 
 // app.use(cors({
-//   origin: function(origin, callback){
-//     // allow requests with no origin 
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){
-//       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   },
-//   credentials: true
-// }));
+
 
 
 app.use(cors({
